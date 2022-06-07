4c1c88b264b18da6c71083c80106da0d98e0597c
kggold4 <68167734+kggold4@users.noreply.github.com>
2022-06-08 00:17:18 +0300

    * performance ex9 update

--- prtpy/partitioning/irnp.py
+++ prtpy/partitioning/irnp.py
@@ -12,56 +12,19 @@
     Email: kfir.goldfarb@msmail.ariel.ac.il
 """
 import copy
-import itertools
+import threading
+import time
 from typing import Callable, List
 from prtpy import Bins
 from prtpy.bins import BinsKeepingContents
 from prtpy.partitioning.ckk import ckk
 from prtpy.partitioning.rnp import rnp
-from prtpy.utils import base_check_bins, is_all_lists_are_different, all_in, get_best_best_k_combination, max_largest, \
-    get_best_partition
+from prtpy.utils import base_check_bins, max_largest, get_best_partition
 
 
-def irnp(bins: Bins, items: List[any], valueof: Callable = lambda x: x):
+def irnp(bins: Bins, items: List[any], valueof: Callable = lambda x: x, with_threads: bool = False):
     """
     Partition the numbers using the improved recursive number partitioning algorithm
-
-    >>> from prtpy.bins import BinsKeepingContents, BinsKeepingSums
-    >>> list(irnp(BinsKeepingContents(2), items=[18, 17, 12, 11, 8, 2]).sums)
-    [36.0, 32.0]
-
-    >>> irnp(BinsKeepingContents(2), items=[95, 15, 75, 25, 85, 5]).bins
-    [[85, 75], [95, 25, 15, 5]]
-
-    >>> irnp(BinsKeepingContents(3), items=[5, 8, 6, 4, 7]).bins
-    [[8], [7, 4], [6, 5]]
-
-    >>> list(rnp(BinsKeepingContents(3), items=[95, 15, 75, 25, 85, 5]).sums)
-    [100.0, 100.0, 100.0]
-
-    >>> rnp(BinsKeepingContents(2), items=[1, 6, 2, 3, 4, 7]).bins
-    [[7, 3, 2], [6, 4, 1]]
-
-    >>> rnp(BinsKeepingContents(2), items=[18, 17, 12, 11, 8, 2]).bins
-    [[17, 11, 8], [18, 12, 2]]
-
-    >>> list(rnp(BinsKeepingContents(2), items=[95, 15, 75, 25, 85, 5]).sums)
-    [160.0, 140.0]
-
-    >>> rnp(BinsKeepingContents(4), items=[3, 6, 13, 20, 30, 40, 73]).bins
-    [[73], [40], [30, 6], [20, 13, 3]]
-
-    >>> rnp(BinsKeepingContents(5), items=[1, 2, 3, 4, 5]).bins
-    [[1], [2], [3], [4], [5]]
-
-    >>> rnp(BinsKeepingContents(2), items=[1, 2]).bins
-    [[1], [2]]
-
-    >>> rnp(BinsKeepingContents(1), items=[1, 6, 2, 3, 4, 7]).bins
-    [[1, 6, 2, 3, 4, 7]]
-
-    >>> rnp(BinsKeepingContents(0), items=[number for number in range(10)]).bins
-    []
 
     """
     k = bins.num
@@ -70,14 +33,43 @@ def irnp(bins: Bins, items: List[any], valueof: Callable = lambda x: x):
         return bins
     items.sort(reverse=True, key=valueof)
     max_largest_bins = max_largest(bins=copy.deepcopy(bins), items=items, valueof=valueof)
-    rnp_bins = rnp(bins=copy.deepcopy(bins), items=items, valueof=valueof)
-    ckk_bins = ckk(bins=copy.deepcopy(bins), items=items, valueof=valueof)
-    perfect_solution = get_best_partition([max_largest_bins, rnp_bins, ckk_bins], k)
-    return perfect_solution
+
+    if with_threads:
+
+        rnp_bins = copy.deepcopy(bins)
+        t1 = threading.Thread(target=rnp, args=[rnp_bins, items, valueof])
+
+        ckk_bins = copy.deepcopy(bins)
+        t2 = threading.Thread(target=ckk, args=[ckk_bins, items, valueof])
+
+        start1 = time.time()
+        t1.start()
+
+        start2 = time.time()
+        t2.start()
+
+        t1.join()
+        end1 = time.time()
+
+        t2.join()
+        end2 = time.time()
+
+        print(f"time for thread1: {end1 - start1}")
+        print(f"time for thread2: {end2 - start2}")
+        print(f"total time: {end2 - start1}")
+
+        perfect_solution = get_best_partition([max_largest_bins, rnp_bins, ckk_bins], k)
+        return perfect_solution, end1 - start1, end2 - start2, end2 - start1
+
+    else:
+        start1 = time.time()
+        rnp_bins = rnp(bins=copy.deepcopy(bins), items=items, valueof=valueof)
+        ckk_bins = ckk(bins=copy.deepcopy(bins), items=items, valueof=valueof)
+        end1 = time.time()
+        print(f"total time: {end1 - start1}")
+        perfect_solution = get_best_partition([max_largest_bins, rnp_bins, ckk_bins], k)
+        return perfect_solution, end1 - start1
 
 
 if __name__ == "__main__":
-    import doctest
-
-    (failures, tests) = doctest.testmod(report=True)
-    print("{} failures, {} tests".format(failures, tests))
+    irnp(BinsKeepingContents(5), items=[i for i in range(1, 8)], with_threads=True)
